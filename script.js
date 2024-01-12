6356fe55784d7d24544c6e4f663e3e7c7025f6a4
George <geo264@mail.ru>
2024-01-12 10:06:23 +0300

    answered the last question

--- script.js
+++ script.js
@@ -3,10 +3,13 @@
 // 1) Написать ответ - почему массивы в JS являются "неправильными" и совмещают в себе несколько
 // структур данных? Какие ?
 
+//! Массив задумывался неизменным. В JS длинна массива является динамической. В массив можно добавлять, удалять элементы, тем самым изменять длинну.
+//! Есть структуры данных производные от массива: стек и очередь.
+
 // 2) Привязать контекст объекта к функции logger, чтобы при вызове this.item выводило - some value
 // (Привязать через bind, call, apply)
 
-/* function logger() {
+function logger() {
 	console.log(`I output only external context: ${this.item}`);
 }
 const obj = { item: "some value" };
@@ -15,21 +18,18 @@ logger.call(obj);
 logger.apply(obj);
 
 const addObj = logger.bind(obj);
-addObj(); */
+addObj();
 
 // Бонус задание: Реализовать полифил(собственную функцию реализующую встроенную в js) метода bind()
 
-const bind = (fn, context, ...boundArgs) => (...args) => {
-	return fn.apply(context, [...boundArgs, ...args]);
+const bind = (func, context, ...boundArgs) => (...args) => {
+	return func.apply(context, [...boundArgs, ...args]);
 }
 
-// Пример:
-
-function greeting(greeting, punctuation) {
-	return `${greeting} ${this.userName}${punctuation}`;
+function greeting(surname, greeting, punctuation) {
+	return `${greeting} ${this.userName} ${surname} ${punctuation}`;
 }
 
-const alex = { userName: "Alex" };
-const alexBound = bind(greeting, alex);
-
-console.log(alexBound("Hello", "!")); // 'Hello Alex!'
+const user = { userName: "Alex" };
+const userBound = bind(greeting, alex, 'Ivanov');
+userBound("Hello", "!");
